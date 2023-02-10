a6530b9cc70f543022d88e6d7dc0229c927e19ed
kingmusab-tmt <musab.buraimoh@gmail.com>
2023-02-10 18:39:30 +0100

    Initialize project using Create React App

---
+++ nav.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "./logo.png";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/">
+        <img src={logo} alt="Brand Logo" />
+        Brand Name
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link className="nav-link" to="/">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/about">
+              About
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/product">
+              Product
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/faq">
+              FAQ
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/contact">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
